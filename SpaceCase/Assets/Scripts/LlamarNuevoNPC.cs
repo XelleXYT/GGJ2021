b40b41d2c449f5f187a0511578589c76bb5c37bd
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2021-01-31 01:17:18 +0100

    Creado sistema de Spawn de NPCs
    Cambiado tag de los prefab de las pelotas galacticas.

--- SpaceCase/Assets/Scripts/LlamarNuevoNPC.cs
+++ SpaceCase/Assets/Scripts/LlamarNuevoNPC.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class LlamarNuevoNPC : MonoBehaviour
 {
+    public float actionDistance = 1.5f;
+    private Color mouseOverColor = Color.yellow;
+    private Color originalColor = Color.blue;
+    public GameObject npc;
+    public GameObject npcSpawn;
+
+    private void Start()
+    {
+        originalColor = GetComponent<Renderer>().material.color;
+    }
+
+    void OnMouseEnter()
+    {
+        if (Vector3.Distance(transform.position, Camera.main.transform.position) < actionDistance)
+        {
+            GetComponent<Renderer>().material.color = mouseOverColor;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+
     private void OnMouseDown()
     {
-        Debug.Log("Llamar nuevo NPC");
+        if (Vector3.Distance(transform.position, Camera.main.transform.position) < actionDistance)
+        {
+            Debug.Log("Llamar nuevo NPC");
+
+            GameObject NPC = Instantiate(npc, npcSpawn.transform.position, npcSpawn.transform.rotation);
+
+        }
     }
 }
